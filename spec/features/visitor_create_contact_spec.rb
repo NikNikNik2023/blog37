8703a839ebbaa0c083dcb29a0b6c66ee79a0f72b
NikNik2023 <novitskiy_nn@mail.ru>
2024-08-31 15:48:29 +0300

    Update contacts new view

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -4,6 +4,6 @@ feature "Contact Creation" do
   scenario "allows acees to contacts page" do
     visit '/contacts'
 
-    expect(page).to have_content 'Contacts us'
+    expect(page).to have_content 'Contacts Us'
   end
 end
